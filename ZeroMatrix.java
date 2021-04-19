aa0fe36a6831551282ae133a3c2a1e7d426b478a
Eltayeb Abdelrahman <eabdelra@umd.edu>
2021-04-19 08:11:22 -0400

    Create ZeroMatrix.java
    completed problem 1.8

---
+++ ZeroMatrix.java
@@ -0,0 +1,57 @@
+public class ZeroMatrix {
+    public static void main(String[] args) {
+		int nrows = 10;
+		int ncols = 15;
+		int[][] matrix = AssortedMethods.randomMatrix(nrows, ncols, -10, 10);		
+
+		AssortedMethods.printMatrix(matrix);
+		
+		setZeros(matrix);
+		
+		System.out.println();
+		
+		AssortedMethods.printMatrix(matrix);
+    }
+
+    public static void setZeros(int[][] matrix) {
+        boolean[] row = new boolean[matrix.length];
+        boolean[] column = new boolean[matrix[0].length];
+
+        // Store the row and column index with value 0
+        for (int i = 0; i < row.length; i++) {
+            for (int j = 0; j < column.length; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    column[j] = true;
+                }
+            }
+        }
+
+        // nullify rows: making it = to null
+        for (int i = 0; i < row.length; i++) {
+            if(row[i]){
+                nullifyRow(matrix, i);
+            }
+        }
+
+        // nullify columns: making it = to null
+        for (int j = 0; j < column.length; j++) {
+            if(column[j]){
+                nullifyColumn(matrix, j);
+            }
+        }
+    }
+
+    public static void nullifyRow(int[][] matrix, int row) {
+        for (int i = 0; i < matrix[0].length; i++) {
+            matrix[row][i] = 0;
+        }    
+    }
+
+    public static void nullifyColumn(int[][] matrix, int col) {
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][col] = 0;
+        }    
+    }
+
+}
